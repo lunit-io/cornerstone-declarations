aae4c01fdcdf4587701b8015cd2b412875775cd6
SSen <i@ssen.name>
2020-04-03 14:55:48 +0900

    feat: update types of cornerstone-wado-image-loader feat: declare modules by package names chore: prettier config

--- cornerstone-wado-image-loader.d.ts
+++ cornerstone-wado-image-loader.d.ts
@@ -1,27 +1,31 @@
 /// <reference path="./cornerstone-core.d.ts"/>
 /// <reference path="./dicom-parser.d.ts"/>
 
+declare module cornerstoneWADOImageLoader {
+  export interface WebWorkerConfig {
+    maxWebWorkers?: number;
+    startWebWorkersOnDemand?: boolean;
+    webWorkerTaskPaths?: string[];
+    taskConfiguration?: {
+      decodeTask?: {
+        initializeCodecsOnStartup?: boolean;
+        usePDFJS?: false;
+        strict?: boolean;
+      };
+      sleepTask?: {
+        sleepTime?: number;
+      };
+    };
+  }
+}
+
 declare const cornerstoneWADOImageLoader: {
   external: {
     cornerstone: typeof cornerstone;
     dicomParser: typeof dicomParser;
   };
   webWorkerManager: {
-    initialize: (config: {
-      maxWebWorkers?: number;
-      startWebWorkersOnDemand?: boolean;
-      webWorkerTaskPaths?: string[];
-      taskConfiguration?: {
-        decodeTask?: {
-          initializeCodecsOnStartup?: boolean;
-          usePDFJS?: false;
-          strict?: boolean;
-        };
-        sleepTask?: {
-          sleepTime?: number;
-        };
-      };
-    }) => void;
+    initialize: (config: cornerstoneWADOImageLoader.WebWorkerConfig) => void;
     terminate: () => void;
   };
   wadouri: {
@@ -31,5 +35,28 @@ declare const cornerstoneWADOImageLoader: {
     fileManager: {
       add: (file: File) => string;
     };
+  };
+  wadors: {
+    metaData: {
+      getNumberString: (element: unknown, index?: number, defaultValue?: string) => unknown;
+      getNumberValue: (element: unknown, index?: number) => unknown;
+      getNumberValues: (element: unknown, minimumLength?: number) => unknown;
+      getValue: (element: unknown, index?: number, defaultValue?: string) => unknown;
+      metaDataProvider: (type: string, imageId: string) => unknown;
+    };
+    findIndexOfString: (data: unknown, str: string, offset?: number) => number;
+    getPixelData: (
+      uri,
+      imageId,
+      mediaType?: string,
+    ) => Promise<{ contentType: string; imageFrame: { pixelData: Uint8Array } }>;
+    loadImage: (imageId: string, options: unknown) => Promise<cornerstone.Image>;
+    metaDataManager: {
+      add: (imageId: string, metadata: unknown) => void;
+      get: (imageId: string) => unknown;
+      remove: (imageId: string) => void;
+      purge: () => void;
+    };
+    register: (_cornerstone: typeof cornerstone) => void;
   };
 };
--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,19 @@
 /// <reference path="./dicom-parser.d.ts"/>
 /// <reference path="./cornerstone-wado-image-loader.d.ts"/>
 /// <reference path="./cornerstone-web-image-loader.d.ts"/>
+
+declare module 'cornerstone-core' {
+  export = cornerstone;
+}
+
+declare module 'cornerstone-wado-image-loader' {
+  export = cornerstoneWADOImageLoader;
+}
+
+declare module 'cornerstone-web-image-loader' {
+  export = cornerstoneWebImageLoader;
+}
+
+declare module 'dicom-parser' {
+  export = dicomParser;
+}
